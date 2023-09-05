beccff7e33aee52bed90dcef70edeba4777dc43a
gunkatanaman <gunkatanaman@gmail.com>
2023-09-06 04:45:17 +0900

    add types

--- lib_types/types.d.ts
+++ lib_types/types.d.ts
@@ -1,19 +1,20 @@
 import type { CSSProperties } from "vue";
+export declare type GanttBarConfig = {
+    id: string;
+    label?: string;
+    hasHandles?: boolean;
+    immobile?: boolean;
+    bundle?: string;
+    pushOnOverlap?: boolean;
+    dragLimitLeft?: number;
+    dragLimitRight?: number;
+    style?: CSSProperties;
+};
 export declare type GanttBarObject = {
     [key: string]: any;
     beginDate: string;
     endDate: string;
-    ganttBarConfig: {
-        id: string;
-        label?: string;
-        hasHandles?: boolean;
-        immobile?: boolean;
-        bundle?: string;
-        pushOnOverlap?: boolean;
-        dragLimitLeft?: number;
-        dragLimitRight?: number;
-        style?: CSSProperties;
-    };
+    ganttBarConfig: GanttBarConfig;
 };
 export declare type Test = {
     d: string;
