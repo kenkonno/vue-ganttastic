f66db3b5988b9479714396582f63ab965acc6f9f
gunkatanaman <gunkatanaman@gmail.com>
2023-09-16 22:37:07 +0900

    export ggtanttlabelrow

--- src/vue-ganttastic.ts
+++ src/vue-ganttastic.ts
@@ -1,15 +1,16 @@
-import type { Plugin } from "vue"
+import type {Plugin} from "vue"
 import dayjs from "dayjs"
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js"
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter.js"
 import isBetween from "dayjs/plugin/isBetween.js"
 import customParseFormat from "dayjs/plugin/customParseFormat.js"
 
-import type { GanttBarObject } from "./types.js"
-import type { ColorScheme } from "./color-schemes"
+import type {GanttBarObject} from "./types.js"
+import type {ColorScheme} from "./color-schemes"
 
 import GGanttChart from "./components/GGanttChart.vue"
 import GGanttRow from "./components/GGanttRow.vue"
+import GGanttLabelRow from "./components/GGanttLabelRow.vue"
 
 export function extendDayjs() {
   dayjs.extend(isSameOrBefore)
@@ -18,14 +19,15 @@ export function extendDayjs() {
   dayjs.extend(customParseFormat)
 }
 
-export type { ColorScheme, GanttBarObject }
-export { GGanttChart, GGanttRow }
+export type {ColorScheme, GanttBarObject}
+export {GGanttChart, GGanttRow, GGanttLabelRow}
 
 export const ganttastic: Plugin = {
   install(app, options?) {
     extendDayjs()
     app.component("GGanttChart", GGanttChart)
     app.component("GGanttRow", GGanttRow)
+    app.component("GGanttLabelRow", GGanttLabelRow)
   }
 }
 
